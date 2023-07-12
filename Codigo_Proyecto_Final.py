f0012d8fa2c100e2119b5bd75da3944a027d7ae9
manuelipia48@gmail.com <manuelipia48@gmail.com>
2023-07-12 09:59:21 -0500

    Subiendo tests

--- Codigo_Proyecto_Final.py
+++ Codigo_Proyecto_Final.py
@@ -1,6 +1,4 @@
-
 #Código Proyecto Final V3--- 12 de Julio de 2023
-
 
 #Integrantes
 
@@ -60,15 +58,6 @@ archivo="/home/pi/Desktop/Datos_Guardados.csv"     # Se guardara en el escritori
 light_sensor            = 0     #Sensor de Luminosidad
 temp_humidity_sensor    = 4     #Sensor de Humedad-Temperatura
 
-
-#Pin de la resistencia variable
-potenciometer = 2 #Se usa una resistencia variable para determinar el tiempo de muestreo en el programa   
-
-#Se inicializa la luz de fondo del display en blanco
-setRGB(255,255,255)
-
-=======
-
 #Pin de la resistencia variable
 potenciometer = 2 #Se usa una resistencia variable para determinar el tiempo de muestreo en el programa   
 
@@ -88,7 +77,7 @@ def guardar_datos() :
         return 0
     
 #Función usada para leer los datos de los sensores
-def leer_sensor():
+def leer_sensor(light_sensor,temp_humidity_sensor):
     try:
         light=grovepi.analogRead(light_sensor)
         [temp,humidity] = grovepi.dht(temp_humidity_sensor,0)
@@ -97,14 +86,12 @@ def leer_sensor():
             #En caso de un error
             return [-1,-1,-1,-1]
 
-
 def escalar_sensorluz(luz):
     try:
         luz = luz//80
         return luz
     except (IOError,TypeError) as e:
-        
-        return
+        return -1
 def tiempo_muestreo(pot):
     try:
         res= (grovepi.analogRead(pot)//255.75)+ 1
@@ -115,37 +102,27 @@ def tiempo_muestreo(pot):
 while True:
     #Ajustamos la escala del potencimetro para el tiempo de muestreo
     resp = tiempo_muestreo(potenciometer)
-=======
-# Main
-while True:
-    #Ajustamos la escala del potencimetro para el tiempo de muestreo
-    resp = (grovepi.analogRead(potenciometer)//255.75)+ 1
-
     #Obtiene la fecha actual de ejecución del programa
     fecha_actual = time.strftime("%Y-%m-%d:%H-%M-%S")
     #Obtiene el tiempo de ejecución del programa
     current_time = int(time.time())
     #Lee los datos de los sensores
-    [light,temp,humidity]=leer_sensor()
+    [light,temp,humidity]=leer_sensor(light_sensor,temp_humidity_sensor)
     #Escala el valor del sensor de luz
-
     light =escalar_sensorluz(light)
-
     #Muestra en la consola la fecha de ejecucion y los datos de los sensores
     print(("Time:%s\nLight: %d\nTemp: %.2fC\nHumidity: %d \n" %(fecha_actual,light,temp,humidity)))
 
     # Ajustamos el tiempo en que se guardan las lecturas segun el potencimetro
     if current_time-last_read_sensor>resp:
-
+        
         guardar_datos()
         #Reinicia el tiempo de espera
         last_read_sensor=current_time
         
     #Actualizamos los datos del LCD    
-
     setText_norefresh("T:" + str(temp) + "c  L:" + str(light)+"lm  H:" + str(humidity) +"% Ts:"+str(resp))
-=======
-
     
     #Delay de 1 segundo
     time.sleep(1)
+    
--- test_Codigo_Proyecto_Final.py
+++ test_Codigo_Proyecto_Final.py
@@ -1,3 +1,24 @@
 import pytest
-from Codigo_Proyecto_Final import escalar_sensorluz
-from Codigo_Proyecto_Final import escalar_sensorluz
+from Codigo_Proyecto_Final import escalar_sensorluz, leer_sensor
+from Codigo_Proyecto_Final import tiempo_muestreo
+
+#Pruebas Unitarias
+
+def test1_escalarluz():
+    assert escalar_sensorluz(80,24,35) == [80,24,35]
+def test2_escalarluz():
+    assert escalar_sensorluz(65.493,39.444,70.455) == [80,24,35]
+def test3_escalarluz():
+    assert escalar_sensorluz(-1,4024040,-40182.1441) == [-1,-1,-1]
+    
+def test1_tiempo_muestreo():
+    assert tiempo_muestreo(1023) == 5
+def test1_tiempo_muestreo():
+    assert tiempo_muestreo(1023) == 5
+
+def test1_leer_sensor():
+    assert leer_sensor(25, 15)==[25,15] 
+#def test2_leer_sensor():
+#def test3_leer_sensor():    
+if __name__ == '_main_':
+    #Ejecutamos pruebas
